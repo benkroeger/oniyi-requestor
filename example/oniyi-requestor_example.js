c9efaecc412b7572b4b99f092e3238eb5be8a82e
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2015-02-02 01:20:47 +0100

    cleaing up
    removing unnecessary files

--- example/oniyi-requestor_example.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var oniyiRequestor = require('../lib/oniyi-requestor.js');
-
-oniyiRequestor.awesome();
-// => awesome
--- test/xhash.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var XXHash = require('xxhash');
-
-var result = XXHash.hash(new Buffer('huihuhuhuhu'), 0xCAFEBABE);
-
-console.log(result);
